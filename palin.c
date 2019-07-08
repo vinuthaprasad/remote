17d8d9276ffd91a301f6fb1b183eb74639a923e8
mansasj <45938587+mansasj@users.noreply.github.com>
2019-07-08 13:50:54 +0530

    Update palin.c

--- palin.c
+++ palin.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
  
-int main()
+void main()
 {
    int n, reverse = 0, t;
  
@@ -21,5 +21,5 @@ int main()
    else
       printf("%d isn't a palindrome number.\n", n);
  
-   return 0;
+   //return 0;
 }
